e7c1ea4b5816d81f9f7f4326da2ff4b2b3f891c9
Vitaliy Melnik <vitaliymelnik9@gmail.com>
2014-03-19 22:02:10 +0200

    delete files

--- themes/incafe/blocks/base/block_mainMenu.php
+++
@@ -1,6 +0,0 @@
-<ul style="list-style: none;  font-weight: bolder; margin:  0; ">
-    <li>Новости</li>
-    <li>Рекомендации</li>
-    <li>Подписки</li>
-    <li style="text-align: center">...</li>
-</ul>
